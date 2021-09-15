e297dd70eb39febf112c65b5fbe2a93ebc0d881a
Nutation-chn <73577878+Nutation-chn@users.noreply.github.com>
2021-09-15 15:24:19 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/zhan0703/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhan0703/MainActivity.java
@@ -3,6 +3,12 @@ package algonquin.cst2335.zhan0703;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button btn = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                String editString = myedit.getText().toString();
+//                mytext.setText( "Your edit text has: " + editString);
+//            }
+//        });
+        btn.setOnClickListener( vw  -> {  mytext.setText("Your edit text has: " + myedit.getText().toString());  }   );
+
+        ImageView myimage = findViewById(R.id.logo_algonquin);
+
+        ImageButton imgbtn = findViewById( R.id.myimagebutton );
+        imgbtn.setOnClickListener( vw  -> {  mytext.setText("The width= " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight()); }   );
+
     }
 }
